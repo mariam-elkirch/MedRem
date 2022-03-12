08b01932852c4cce6ce555cd1de99ef12d587d71
Abdelrahman Hamza <64665314+abdelrahmanhz@users.noreply.github.com>
2022-03-12 17:32:11 +0200

    Add MedicationsList, HealthTakersList, DependantsList and RequestsList Screens.

---
+++ app/src/main/java/com/example/medred/requestsList/view/RequestsListFragment.java
@@ -0,0 +1,35 @@
+package com.example.medred.requestsList.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.medred.R;
+
+
+public class RequestsListFragment extends Fragment {
+
+
+    public RequestsListFragment() {
+
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_requests_list, container, false);
+        return view;
+    }
+}
